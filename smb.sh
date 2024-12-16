acb3c57563cbbb372376a949e41de40169c27808
cyberralf83 <ralf83@gmail.com>
2024-12-16 16:35:21 -0500

    Update smb.sh

--- smb.sh
+++ smb.sh
@@ -1,48 +1,37 @@
-sudo bash -c 'cat > /tmp/install_smb.sh << "EOL"
 #!/bin/bash
 
-# Exit on any error
+# Exit on error
 set -e
 
-# Update package list
+# Update package list and install samba
 apt-get update
+apt-get install -y samba
 
-# Install Samba 
-apt-get install -y samba samba-common-bin
-
-# Backup original config
+# Backup the original smb.conf
 cp /etc/samba/smb.conf /etc/samba/smb.conf.backup
 
+# Get logged in user
 LOGGED_IN_USER=$(logname)
-SMB_CONF="/etc/samba/smb.conf"
 
 echo "The user logged in is: $LOGGED_IN_USER"
 
-systemctl restart smbd
-systemctl restart nmbd
+# Find and modify the [homes] section to add inherit permissions and writable
+sed -i '/\[homes\]/,/\[/ s/$/\n   inherit permissions = yes\n   writable = yes/' /etc/samba/smb.conf
 
+# Restart samba to apply changes
+systemctl restart smbd
+
+# Set up Samba password for the logged-in user
 USER=$(logname)
 sudo smbpasswd -a $USER
 
+# Restart samba to apply changes
 systemctl restart smbd
-systemctl restart nmbd
 
-# Enable Samba services to start on boot
-systemctl enable smbd
-systemctl enable nmbd
-
-# Configure firewall if UFW is installed
-if command -v ufw >/dev/null 2>&1; then
-    ufw allow Samba
-fi
-
-echo "Samba installation complete!"
-echo "Users can now connect using their system username and password"
+# Print network info and SMB URL
 IP_ADDR=$(hostname -I | awk '{print $1}')
 HOSTNAME=$(hostname | awk '{print}')
 USERNAME=$(logname)
 echo "Samba is now configured. Connect using your current username and password."
 echo "Your SMB URL is: smb://$IP_ADDR/$USERNAME"
 echo "Your SMB URL is: smb://$HOSTNAME.local/$USERNAME"
-EOL
-chmod +x /tmp/install_smb.sh && /tmp/install_smb.sh && rm /tmp/install_smb.sh'
