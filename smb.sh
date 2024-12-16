de381bc07128d3b633154fc912035a1063f5c04e
cyberralf83 <ralf83@gmail.com>
2024-12-16 16:16:00 -0500

    Update smb.sh

--- smb.sh
+++ smb.sh
@@ -7,58 +7,27 @@ set -e
 # Update package list
 apt-get update
 
-# Install Samba and required packages including PAM authentication
-apt-get install -y samba samba-common-bin libpam-winbind
+# Install Samba 
+apt-get install -y samba samba-common-bin
 
 # Backup original config
 cp /etc/samba/smb.conf /etc/samba/smb.conf.backup
 
-# Create new Samba configuration
-cat > /etc/samba/smb.conf << "EOF"
-[global]
-   workgroup = WORKGROUP
-   server string = Samba Server
-   security = user
-   map to guest = never
-   dns proxy = no
-   
-   # Enable PAM authentication
-   pam password change = yes
-   unix password sync = yes
-   passwd program = /usr/bin/passwd %u
-   passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
-   
-   # Use system users and passwords
-   passdb backend = tdbsam
-   obey pam restrictions = yes
-   
-   # Logging configuration
-   log file = /var/log/samba/log.%m
-   max log size = 1000
-   logging = file
-   
-   # Protocol configurations
-   server min protocol = SMB2
-   server max protocol = SMB3
-   
-   # Performance tuning
-   socket options = TCP_NODELAY IPTOS_LOWDELAY
-   read raw = yes
-   write raw = yes
-   
-[homes]
-   comment = Home Directories
-   browseable = no
-   read only = no
-   create mask = 0666
-   force create mode = 0666
-   directory mask = 0777
-   force directory mode = 0777
-   valid users = %S
-   inherit permissions = yes
-EOF
+LOGGED_IN_USER=$(logname)
 
-# Restart Samba services
+echo "The user logged in is: $LOGGED_IN_USER"
+
+
+sed -i '/\[homes\]/,/\[/ s/^[[:space:]]*read only[[:space:]]*=[[:space:]]*yes/\tread only = no/' /etc/samba/smb.conf
+sed -i '/\[homes\]/a\   valid users = %S\n   inherit permissions = yes' /etc/samba/smb.conf
+
+
+systemctl restart smbd
+systemctl restart nmbd
+
+USER=$(logname)
+sudo smbpasswd -a $USER
+
 systemctl restart smbd
 systemctl restart nmbd
 
@@ -73,7 +42,11 @@ fi
 
 echo "Samba installation complete!"
 echo "Users can now connect using their system username and password"
-echo "Your SMB URL is: smb://$HOSTNAME.local"
-echo "Your SMB URL is: smb://$IP_ADDR"
+IP_ADDR=$(hostname -I | awk '{print $1}')
+HOSTNAME=$(hostname | awk '{print}')
+USERNAME=$(logname)
+echo "Samba is now configured. Connect using your current username and password."
+echo "Your SMB URL is: smb://$IP_ADDR/$USERNAME"
+echo "Your SMB URL is: smb://$HOSTNAME.local/$USERNAME"
 EOL
 chmod +x /tmp/install_smb.sh && /tmp/install_smb.sh && rm /tmp/install_smb.sh'
